63a54a6ef5e8fa8cff1e8016b9c64fd637469ac3
Yuchen Ying <yegle@uga.edu>
2012-09-11 15:44:24 -0400

    Update: add javadoc and update MessageImpl

--- pg2/src/socket/Message.java
+++ pg2/src/socket/Message.java
@@ -1,7 +1,23 @@
+/**
+ * interface that we need to implement
+ */
 public interface Message {
+    /**
+     * set the counts into internal member variables
+     */
     public void setCounts();
 
+    /**
+     * get the character count
+     *
+     * @return number of characters in this instance
+     */
     public int getCharacterCount();
 
+    /**
+     * get the digit count
+     *
+     * @return number of digit in this instance
+     */
     public int getDigitCount();
 }
