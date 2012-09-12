7064dfa2b0963a64834e2418c32b7def887d26fc
Yuchen Ying <yegle@uga.edu>
2012-09-11 21:16:30 -0400

    Updated: lots update

--- pg2/src/rmi/Client.java
+++ pg2/src/rmi/Client.java
@@ -1,20 +1,40 @@
 import java.rmi.*;
+import java.io.*;
 
-public class RMIClient {
- public static void getRemoteObject() throws Exception{ 
-  IRemote obj = (IRemote)Naming.lookup("rmi://127.0.0.1:1111/mytask");
-  Task task = new Task(); 
-  task.doWork();
-        Object result = obj.invoke(task);
-       
-    } 
+public class Client {
+    protected Server server;
+    protected String url;
 
-    /** 
-     * @param args 
-     * @throws Exception 
-     */ 
-    public static void main(String[] args) throws Exception { 
-     RMIClient.getRemoteObject();
-    } 
+    public Client(String url){
+        this.url = url;
+    }
 
+    public static void main(String[] args){
+        try{
+            Client c = new Client("rmi://127.0.0.1/RMI");
+            c.connect();
+
+            System.err.println("Enter the string to be processed");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+            c.process(input);
+
+            c.printResult();
+        } catch(Exception e){
+            System.err.println( "Exception catched, message: " + e.toString());
+        }
+    }
+
+    protected void connect() throws Exception {
+        this.server = (Server) Naming.lookup(url);
+    }
+
+    protected void process(String input){
+        this.server.setInput(input);
+        this.server.setCounts();
+    }
+
+    protected void printResult(){
+        System.out.println("Result received: CharacterCount=" + this.server.getCharacterCount() + ", DigitCount=" + this.server.getDigitCount());
+    }
 }
