56cfd299a4472e94eb8279dd648e7c640e739e58
Yuchen Ying <yegle@uga.edu>
2012-09-11 23:34:00 -0400

    Update: javadoc

--- pg2/src/rmi/ServerImpl.java
+++ pg2/src/rmi/ServerImpl.java
@@ -1,16 +1,36 @@
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.*;
 
+/**
+ * Main server class
+ */
 public class ServerImpl extends UnicastRemoteObject implements Server {
+    /**
+     * internal variable used to save user's input
+     */
     protected String Input;
+    /**
+     * internal variable used to save result
+     */
     protected int CharacterCount, DigitCount;
 
+    /**
+     * empty constructor
+     */
     public ServerImpl() throws Exception{}
 
+    /**
+     * function used to save user's input
+     *
+     * @param input String, User's input
+     */
     public void setInput(String input) throws Exception {
         this.Input = input;
     }
 
+    /**
+     * function to do the real computing
+     */
     public void setCounts() throws Exception {
         int ret = 0;
         for (int i=0; i< this.Input.length(); i++){
@@ -24,14 +44,27 @@ public class ServerImpl extends UnicastRemoteObject implements Server {
         this.CharacterCount = this.Input.length();
     }
 
+    /**
+     * getter for DigitCount
+     *
+     * @return this.DigitCount
+     */
     public int getDigitCount() throws Exception {
         return this.DigitCount;
     }
 
+    /**
+     * getter for CharacterCount
+     *
+     * @return this.CharacterCount
+     */
     public int getCharacterCount() throws Exception {
         return this.CharacterCount;
     }
 
+    /**
+     * main function to run server
+     */
     public static void main(String[] args) {
         try {
             Server s = new ServerImpl();
