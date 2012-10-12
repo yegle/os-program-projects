8449f5a981a8e339a4504b1c894438121c771e1f
Yuchen Ying <yegle@uga.edu>
2012-10-12 16:30:07 -0400

    Update: code cleanup

--- pg4/src/Dot.java
+++ pg4/src/Dot.java
@@ -1,64 +1,65 @@
 //import java.util.concurrent.Semaphore;
 
 public class Dot{
-	//private static int[] sum = new int[10];
+    //private static int[] sum = new int[10];
 
 
-	public static void main(String[] args){
-		//read data from files and assign values to a,b
-		for(int j=0;j<Global.size;j++){
-			Global.a[j]=j;
-			Global.b[j]=j;
-		}
+    public static void main(String[] args){
+        //read data from files and assign values to a,b
+        for(int j=0;j<Global.size;j++){
+            Global.a[j]=j;
+            Global.b[j]=j;
+        }
 
-		//create thread to multiply
-		int tsize = 10;
-		Thread[] mul = new Thread[tsize];
+        //create thread to multiply
+        int tsize = 10;
+        Thread[] mul = new Thread[tsize];
 
-		for(int j=0;j<tsize;j++){
-			//System.err.println(j);
-			mul[j]=new Thread(new Multiplier(j));
+        for(int j=0;j<tsize;j++){
+            //System.err.println(j);
+            mul[j]=new Thread(new Multiplier(j));
 
-		}
+        }
 
-		for(int j=0;j<tsize;j++){
-			System.err.println(j);
-			mul[j].start();
-		}
+        for(int j=0;j<tsize;j++){
+            System.err.println(j);
+            mul[j].start();
+        }
 
-		//create thread to add
-		/*Thread[] add = new Thread[tsize];
+        //create thread to add
+        /*Thread[] add = new Thread[tsize];
 
-		for(int j=0;j<tsize;j++){
-			//System.err.println(j);
-			add[j]=new Thread(new Multiplier(j));
+          for(int j=0;j<tsize;j++){
+        //System.err.println(j);
+        add[j]=new Thread(new Multiplier(j));
 
-		}
+          }
 
-		for(int j=0;j<tsize;j++){
-			System.err.println(j);
-			add[j].start();
-		}*/
+          for(int j=0;j<tsize;j++){
+          System.err.println(j);
+          add[j].start();
+          }*/
 
-	}
-
-
+    }
 
 
-	/*static class adder implements Runnable{
-		private int i;
 
-		adder(int id){
-			i=id;
-		}
 
-		public void run(){
-			try{
-				sum+=c[i];
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-	}*/
+    /*static class adder implements Runnable{
+      private int i;
+
+      adder(int id){
+      i=id;
+      }
+
+      public void run(){
+      try{
+      sum+=c[i];
+      }catch(Exception e){
+      e.printStackTrace();
+      }
+      }
+      }*/
 
 }
+// vim: set ts=4 sw=4 tw=0 :
