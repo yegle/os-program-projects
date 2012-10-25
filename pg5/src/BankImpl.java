6ba237fbce3f5571cc972aec3a2870847cad8bb9
yinyueacm <yinyue@gmail.com>
2012-10-25 17:18:13 -0400

    ~

---
+++ pg5/src/BankImpl.java
@@ -0,0 +1,10 @@
+class BankImpl implements Bank{
+	public static int numberOfCustimers;
+	public static int numberOfResources;
+
+	public static int[] available;
+	public static int[][] maximum;
+	public static int[][] allocation;
+
+	public static int[][] need;
+}
