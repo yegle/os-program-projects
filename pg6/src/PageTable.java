bebf49794d9bfc3ba38700ea7847eb424e14da8d
Yuchen Ying <yegle@uga.edu>
2012-11-10 02:45:40 -0500

    Added: java doc

--- pg6/src/PageTable.java
+++ pg6/src/PageTable.java
@@ -1,6 +1,14 @@
+/**
+ * Class to emulate a page table
+ */
 public class PageTable{
     PageTableItem[] table;
 
+    /**
+     * Constructor
+     *
+     * initialize PageTable with 256 items
+     */
     public PageTable(){
         table = new PageTableItem[256];
         for(int i=0;i<256;i++){
@@ -8,6 +16,12 @@ public class PageTable{
         }
     }
 
+    /**
+     * function to get frame number using page number
+     *
+     * @param int p_num Page Number to be queried
+     * @return int fraame number if it's in table, -1 otherwise
+     */
     public int get(int p_num){
         int frameNumber = this.table[p_num].getFrameNumber();
         if(frameNumber == -1){
@@ -17,20 +31,51 @@ public class PageTable{
         return frameNumber;
     }
 
+    /**
+     * function to add page number, frame number to PageTable
+     *
+     * @param p_num int Page number
+     * @param f_num int Frame number
+     */
     public void add(int p_num, int f_num){
         this.table[p_num] = new PageTableItem(f_num, true);
     }
 }
 
+/**
+ * class for storing items of PageTable
+ *
+ * Because Java doesn't have struct, we use this class
+ * to emulate struct in C.
+ */
 class PageTableItem{
+    /**
+     * variable to store frame number information
+     */
     int frameNumber;
+    /**
+     * variable to store if the frame is in memory
+     *
+     * not used in this project
+     */
     boolean valid;
 
+    /**
+     * Constructor
+     *
+     * @param i int frame number
+     * @param b boolean whether it's valid frame
+     */
     public PageTableItem(int i, boolean b){
         this.frameNumber = i;
         this.valid = b;
     }
 
+    /**
+     * Get the frame number of this PageTable item
+     *
+     * @return this.frameNumber
+     */
     public int getFrameNumber(){
         return this.frameNumber;
     }
--- pg6/src/PhysicalMemory.java
+++ pg6/src/PhysicalMemory.java
@@ -1,18 +1,44 @@
+/**
+ * Class to emulate the physical memory
+ */
 public class PhysicalMemory{
+    /**
+     * variable to emulate frames in memory
+     */
     Frame[] frames;
+    /**
+     * we need a variable to store how many
+     * frames are used
+     */
     int currentFreeFrame;
 
+    /**
+     * Constructor
+     */
     public PhysicalMemory(){
         this.frames = new Frame[256];
         this.currentFreeFrame = 0;
     }
 
+    /**
+     * function to add a new frame to memory
+     *
+     * @param f Frame to be added
+     * @return int the frame number just added
+     */
     public int addFrame(Frame f){
         this.frames[this.currentFreeFrame] = new Frame(f.data);
         this.currentFreeFrame++;
         return this.currentFreeFrame-1;
     }
 
+    /**
+     * function to get value in memory
+     *
+     * @param f_num int frame number
+     * @param offset int offset
+     * @return int content in specified location
+     */
     public int getValue(int f_num, int offset){
         Frame frame = this.frames[f_num];
         return frame.data[offset];
@@ -20,9 +46,20 @@ public class PhysicalMemory{
 
 }
 
+/**
+ * wrapper class to group all frame related logics
+ */
 class Frame {
+    /**
+     * variable to store datas of this frame
+     */
     int[] data;
 
+    /**
+     * Constructor
+     *
+     * @param d int[256] for initializing frame
+     */
     public Frame(int[] d){
         this.data = new int[256];
         for(int i=0;i<256;i++){
@@ -30,6 +67,11 @@ class Frame {
         }
     }
 
+    /**
+     * Copy Constructor
+     *
+     * @param f Frame to be copied
+     */
     public Frame(Frame f){
         this.data = new int[256];
         System.arraycopy(f.data, 0, this.data, 0, 256);
