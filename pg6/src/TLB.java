a74a33f99a88a57e1025d4ef3175ff4ec2ce4a21
Yuchen Ying <yegle@uga.edu>
2012-11-09 23:30:54 -0500

    Update: working TLB

--- pg6/src/TLB.java
+++ pg6/src/TLB.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import java.lang.*;
 
 public class TLB{
     Hashtable table;
+    LinkedList <Integer> list = new LinkedList<Integer>();
 
     public TLB(){
         this.table = new Hashtable();
         for(int i=0; i<16; i++){
             this.table.put(-i, -1);
+            this.list.add(-i);
         }
     }
 
@@ -17,5 +20,33 @@ public class TLB{
         else{
             return -1;
         }
+    }
+
+    public void put(int p_num, int f_num){
+        // we need to do two things:
+        // 1. delete the oldest data in TLB
+        // 2. insert new data
+        //
+        // oldest is determined by the Queue this.list
+
+
+        // get the first item in queue
+        // may return null, so we needs Integer
+        Integer i = this.list.poll();
+        if(i != null){
+            // remove this item from hashtable
+            this.table.remove(i.intValue());
+        }
+
+        this.list.add(p_num);
+        this.table.put(p_num, f_num);
+    }
+
+    public static void main(String[] args){
+        TLB tlb = new TLB();
+        for(int i=0; i<=16; i++){
+            tlb.put(i,i);
+        }
+        System.out.println(tlb.get(0));
     }
 }
