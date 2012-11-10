0ac8b78f99592dfaf16de7f9cd14bd079e4faa76
yinyueacm <yinyue@gmail.com>
2012-11-09 21:13:56 -0500

    addresstranslator

--- pg6/src/AddressTranslator.java
+++ pg6/src/AddressTranslator.java
@@ -9,10 +9,10 @@ public class AddressTranslator {
 
 		try{
 			BufferedReader inFile = new BufferedReader(new FileReader(inputFile));
-
+			String line;
 			int addr;
-			while((inFile.readLine())!= null){
-				addr = Integer.valueOf(inFile.readLine());
+			while((line = inFile.readLine())!= null){
+				addr = Integer.valueOf(line);
 				addr = addr<<8;
 				addr = addr>>8;
 				System.out.println(addr);
