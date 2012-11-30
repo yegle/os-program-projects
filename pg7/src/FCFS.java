6ce6bc53056067716c882b79bf3a6a9e59ee2e4b
yinyueacm <yinyue@gmail.com>
2012-11-30 18:53:57 -0500

    FCFS.java

---
+++ pg7/src/FCFS.java
@@ -0,0 +1,33 @@
+class FCFS implements DiskScheduler{
+	
+	private static int[] rString;
+	private static int start;
+
+	private static int sum;
+
+
+	public FCFS(int[] rS, int s){
+		this.rString = new int[rS.length];
+		System.arraycopy(rS,0,rString,0,rS.length);
+		this.start = s;
+		this.sum=0;
+	}
+
+	public int serviceRequests(){
+		sum += rString[1] - start;
+		for(int i=1;i<rString.length;i++){
+			sum += rString[i];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args){
+        Generator ref = new Generator();
+
+		int[] referenceString = ref.getCylinders();
+		DiskScheduler fcfs = new FCFS(referenceString, 13);
+
+		System.out.println("FCFS = " + fcfs.serviceRequests());
+    }
+}
