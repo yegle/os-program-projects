7ed95f2a372da5c6689f34eec897e4aac6581860
yinyueacm <yinyue@gmail.com>
2012-11-30 22:52:15 -0500

    Scan

---
+++ pg7/src/SCAN.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+
+class SCAN implements DiskScheduler{
+	
+	private static int[] rString;
+	private static int start;
+
+	private static int sum;
+
+
+	public SCAN(int[] rS, int s){
+		this.rString = new int[rS.length];
+		System.arraycopy(rS,0,rString,0,rS.length);
+		this.start = s;
+		this.sum=0;
+	}
+
+	public int serviceRequests(){
+		int pos;
+		int currentPos = this.start;
+		int flag=1;
+		int count=0;
+
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		for(int i=0;i<this.rString.length;i++){
+			list.add(new Integer(this.rString[i]));
+		}
+		while((pos=this.findNearestPos(list, currentPos))>=0){
+			sum += Math.abs(currentPos-pos);
+			currentPos = pos;
+			count++;
+		}
+
+		sum += currentPos;
+		currentPos = 0;
+		for(int i=0;i<this.rString.length-count;i++){
+			pos = this.findNearestPos2(list, currentPos);
+            sum += Math.abs(currentPos-pos);
+            currentPos = pos;
+		}
+		
+
+		return sum;
+	}
+
+    private int findNearestPos(ArrayList<Integer> list, int currentPos){
+        int minDistance = Math.abs(currentPos-0);
+        int distance;
+        int pos = 0;
+        int counter = 0;
+		int flag = 0;
+		int ret = 0;
+        for(int i : list){
+            distance = currentPos-i;
+            if((distance>=0)&&(distance<minDistance)){
+                minDistance = distance;
+                pos = counter;
+				flag = 1;
+            }
+            counter++;
+        }
+		if(flag==1){
+			ret = list.get(pos);
+        	list.remove(pos);
+		}
+		else
+			ret=-1;
+
+       	return ret;
+    }
+
+	private int findNearestPos2(ArrayList<Integer> list, int currentPos){
+        int minDistance = Math.abs(currentPos-0);
+        int distance;
+        int pos = 0;
+        int counter = 0;
+		int ret = 0;
+        for(int i : list){
+            distance = Math.abs(currentPos-i);
+            if((distance<minDistance)){
+                minDistance = distance;
+                pos = counter;
+            }
+            counter++;
+        }
+		ret = list.get(pos);
+        list.remove(pos);
+        return ret;
+
+    }
+
+	public static void main(String[] args){
+        //Generator ref = new Generator();
+
+		//int[] referenceString = ref.getCylinders();
+		int[] referenceString = {98,183,37,122,14,124,65,67};
+		DiskScheduler scan = new FCFS(referenceString, 53);
+
+		System.out.println("SCAN = " + scan.serviceRequests());
+    }
+}
