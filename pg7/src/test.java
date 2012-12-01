4399ff040eb1af4c42431cf62338dc33fa64de43
Yuchen Ying <yegle@uga.edu>
2012-11-30 23:48:26 -0500

    Update: use random reference string

--- pg7/src/test.java
+++ pg7/src/test.java
@@ -1,9 +1,9 @@
 class test{
 	public static void main(String[] args){
-        //Generator ref = new Generator();
+        Generator ref = new Generator();
 
-		//int[] referenceString = ref.getCylinders();
-		int[] referenceString = {98,183,37,122,14,124,65,67};
+		int[] referenceString = ref.getCylinders();
+		//int[] referenceString = {98,183,37,122,14,124,65,67};
 		DiskScheduler fcfs = new FCFS(referenceString, 53);
 		System.out.println("FCFS = " + fcfs.serviceRequests());
         DiskScheduler sstf = new SSTF(referenceString, 53);
